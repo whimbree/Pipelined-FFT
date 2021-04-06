d64f61d861b80f5c18e2b74cc644e621c6028204
Boris Ermakov-Spektor <b.ermakovspektor@ufl.edu>
2021-04-06 00:10:17 -0400

    Twiddle gen now creates user_pkg, uses array of std_logic_vector

--- twiddle_gen.py
+++ twiddle_gen.py
@@ -1,50 +1,98 @@
 from string import Template
 import math
 
-# real: cos(2pi*k / FFT_PTS)
-# img: sin(2pi*k / FFT_PTS)
+# real: cos(-2pi*k / FFT_PTS)
+# img: sin(-2pi*k / FFT_PTS)
 # shift left by data width - 1
 # (above is assuming that we are using fixed point with one sign bit and the rest as fractional bits)
 
 
 def main():
+
+    twiddle_factors_per_line = 10
+    filename = "user_pkg.vhd"
+
     num_points = int(input("How many points is the FFT?: "))
     data_width = int(input("What is the data width?: "))
 
-    constant_template = 'constant $key  : std_logic_vector(DATA_RANGE) := x"$value";'
-    constant_template_obj = Template(constant_template)
+    array_value_template = '$index => x"$value"'
+    array_value_template = Template(array_value_template)
 
-    print()
+    with open(filename, "w") as f:
 
-    for k in range(0, num_points):
-        real_value = math.cos(2 * math.pi * k / num_points)
-        real_value *= 2**(data_width - 1)
-        real_value = int(real_value) & (2**data_width - 1)
-        # Prevent 0x8000... from causing overflows
-        if real_value == 2**(data_width - 1):
-            real_value -= 1
-        # https://stackoverflow.com/questions/7822956/how-to-convert-negative-integer-value-to-hex-in-python
-        print(
-            constant_template_obj.substitute(
-                key="REAL_" + str(k),
-                value='{:0x}'.format(real_value).zfill(
-                    math.ceil(data_width / 4))))
+        f.write("library ieee;\n"
+                "use ieee.std_logic_1164.all;\n"
+                "use ieee.numeric_std.all;\n\n"
+                "package user_pkg is\n\n")
 
-    print()
+        f.write(
+            f"constant DATA_WIDTH : positive := {data_width};\n" +
+            "subtype DATA_RANGE is natural range DATA_WIDTH - 1 downto 0;\n\n")
 
-    for k in range(0, num_points):
-        imag_value = math.sin(2 * math.pi * k / num_points)
-        imag_value *= 2**(data_width - 1)
-        imag_value = int(imag_value) & (2**data_width - 1)
-        # Prevent 0x8000... from causing overflows
-        if imag_value == 2**(data_width - 1):
-            imag_value -= 1
-        # https://stackoverflow.com/questions/7822956/how-to-convert-negative-integer-value-to-hex-in-python
-        print(
-            constant_template_obj.substitute(
-                key="IMAG_" + str(k),
-                value='{:0x}'.format(imag_value).zfill(
-                    math.ceil(data_width / 4))))
+        f.write(
+            "type TWIDDLE_ARRAY is array (natural range <>) of std_logic_vector(DATA_RANGE);\n"
+            +
+            f"subtype TWIDDLE_RANGE is natural range 0 to {num_points - 1};\n\n"
+        )
+
+        # REAL TWIDDLE FACTOR GENERATION
+
+        f.write(
+            f"constant REAL_TWIDDLE_FACTORS : TWIDDLE_ARRAY(TWIDDLE_RANGE) := ("
+        )
+
+        for k in range(0, num_points):
+            f.write(
+                array_value_template.substitute(
+                    index=k,
+                    value='{:0x}'.format(
+                        gen_twiddle_factor(math.cos, k, num_points,
+                                           data_width)).zfill(
+                                               math.ceil(data_width / 4))))
+            if k != num_points - 1:
+                f.write(",")
+            if (k + 1) % twiddle_factors_per_line == 0:
+                f.write("\n")
+
+        f.write(");\n\n")
+
+        # IMAGINARY TWIDDLE FACTOR GENERATION
+
+        f.write(
+            f"constant IMAG_TWIDDLE_FACTORS : TWIDDLE_ARRAY(TWIDDLE_RANGE) := ("
+        )
+
+        array_value_template = '$index => x"$value"'
+        array_value_template = Template(array_value_template)
+
+        for k in range(0, num_points):
+            f.write(
+                array_value_template.substitute(
+                    index=k,
+                    value='{:0x}'.format(
+                        gen_twiddle_factor(math.sin, k, num_points,
+                                           data_width)).zfill(
+                                               math.ceil(data_width / 4))))
+            if k != num_points - 1:
+                f.write(",")
+            if (k + 1) % twiddle_factors_per_line == 0:
+                f.write("\n")
+
+        f.write(");\n\n")
+
+        f.write("end user_pkg;")
+
+
+def gen_twiddle_factor(trig_func, k, num_points, data_width) -> int:
+    epsilon = 0.0001
+
+    value = trig_func(-2 * math.pi * k / num_points)
+    trunc_value = value * 2**(data_width - 1)
+    # https://stackoverflow.com/questions/7822956/how-to-convert-negative-integer-value-to-hex-in-python
+    trunc_value = int(trunc_value) & (2**data_width - 1)
+    if abs(value - 1.0) < epsilon:
+        trunc_value -= 1
+    return trunc_value
 
 
 if __name__ == "__main__":
